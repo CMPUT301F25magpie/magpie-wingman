ca9f1f770f15a755d99789a1c674d9c28f9dd230
Mason Delamont <mdelamon@ualberta.ca>
2025-11-20 16:17:01 -0700

    Implemented return nav from sign up page

--- code/app/src/main/java/com/example/magpie_wingman/data/SignUpFragment.java
+++ code/app/src/main/java/com/example/magpie_wingman/data/SignUpFragment.java
@@ -1,13 +1,17 @@
 package com.example.magpie_wingman.data;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.magpie_wingman.R;
 
@@ -23,5 +27,12 @@ public class SignUpFragment extends Fragment {
     public void onViewCreated(@NonNull View view,
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+
+        TextView btnSignInText = view.findViewById(R.id.signInText);
+
+        btnSignInText.setOnClickListener(v ->
+                navController.navigate(R.id.action_signUpFragment_to_loginFragment));
     }
 }
