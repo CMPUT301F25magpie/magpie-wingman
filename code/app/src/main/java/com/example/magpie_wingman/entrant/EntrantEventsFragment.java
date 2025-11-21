ec45807997c3d02eefb191de138bae44075dece6
Mason Delamont <mdelamon@ualberta.ca>
2025-11-20 17:36:19 -0700

    Small UI bug fix

--- code/app/src/main/java/com/example/magpie_wingman/entrant/EntrantEventsFragment.java
+++ code/app/src/main/java/com/example/magpie_wingman/entrant/EntrantEventsFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.magpie_wingman.R;
