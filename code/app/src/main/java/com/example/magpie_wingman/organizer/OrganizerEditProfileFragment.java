37ca85f52eea9b7483b2cfd9bf26bc8c77c0260f
Mason Delamont <mdelamon@ualberta.ca>
2025-11-21 22:32:10 -0700

    Implemented organizer settings nav

--- code/app/src/main/java/com/example/magpie_wingman/organizer/OrganizerEditProfileFragment.java
+++ code/app/src/main/java/com/example/magpie_wingman/organizer/OrganizerEditProfileFragment.java
@@ -1,15 +1,26 @@
 package com.example.magpie_wingman.organizer;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.magpie_wingman.R;
 
-public class OrganizerEditProfileFragment extends PreferenceFragmentCompat {
+public class OrganizerEditProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_organizer_edit_profile, container, false);
+    }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
--- code/app/src/main/java/com/example/magpie_wingman/organizer/OrganizerSettingsFragment.java
+++ code/app/src/main/java/com/example/magpie_wingman/organizer/OrganizerSettingsFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.magpie_wingman.R;
@@ -25,5 +29,15 @@ public class OrganizerSettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view,
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ImageView btnEditProfile = view.findViewById(R.id.btn_edit_profile);
+        ImageView btnAboutUs = view.findViewById(R.id.btn_about_us);
+        Button btnLogOut = view.findViewById(R.id.button_logout);
+
+        NavController navController = Navigation.findNavController(view);
+
+        btnEditProfile.setOnClickListener(v -> navController.navigate(R.id.action_organizerSettingsFragment_to_organizerEditProfileFragment2));
+        btnAboutUs.setOnClickListener(v -> navController.navigate(R.id.action_organizerSettingsFragment_to_organizerAboutUsFragment));
+        btnLogOut.setOnClickListener(v -> navController.navigate(R.id.action_organizerSettingsFragment_to_loginFragment));
     }
 }
